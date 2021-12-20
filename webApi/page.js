b52cc139b106555104766750eb3e691a5969e1b5
kyungho <epsoe@naver.com>
2021-12-20 17:06:21 +0900

    Scroll page

--- webApi/page.js
+++ webApi/page.js
@@ -1,5 +1,7 @@
 const box = document.querySelector('.box:nth-child(7)');
-
+const scrollByPx = document.querySelector('.scroll_by');
+const scrollToPx = document.querySelector('.scroll_to');
+const scrollIntoSpecial = document.querySelector('.scroll_special');
 function getPage(e) {
   console.log(box.getBoundingClientRect());
   console.log('client:', e.clientX, e.clientY);
@@ -7,3 +9,14 @@ function getPage(e) {
 }
 
 box.addEventListener('click', getPage);
+
+scrollByPx.addEventListener('click', () => {
+  window.scrollBy(0, 100);
+});
+
+scrollToPx.addEventListener('click', () => {
+  window.scrollTo(0, event.pageY + 100);
+});
+scrollIntoSpecial.addEventListener('click', () => {
+  box.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
+});
