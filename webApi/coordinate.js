092d11f8ce85d14d87aa8f3f5384a33b51d5254b
kyungho <epsoe@naver.com>
2021-12-21 17:18:42 +0900

    Refactoring

--- webApi/coordinate.js
+++ webApi/coordinate.js
@@ -3,15 +3,27 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const mouse = document.querySelector('.mouse');
 
-document.addEventListener('mousemove', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  mouse.style.left = `${x}px`;
-  mouse.style.top = `${y}px`;
-  mouse.innerHTML = `${x}px,${y}px`;
+  document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
+    mouse.style.left = `${x}px`;
+    mouse.style.top = `${y}px`;
+    mouse.innerHTML = `${x}px,${y}px`;
+  });
 });
